9a0d759b712c02b3b80a09397492f42b9886615f
TCAGM-Dev <thomas@andringa.org>
2025-05-26 12:44:47 +0200

    Add hint support

--- src/stratpattern/OpenQuestion.java
+++ src/stratpattern/OpenQuestion.java
@@ -7,16 +7,23 @@ public class OpenQuestion implements Task {
     private String question;
     private String answer;
     private Room room;
+    private String hint;
 
-    public OpenQuestion(String question, String answer, Room room) {
+    public OpenQuestion(String question, String answer, Room room, String hint) {
         this.question = question;
         this.answer = answer;
         this.room = room;
+        this.hint = hint;
+    }
+    public OpenQuestion(String question, String answer, Room room) {
+        this(question, answer, room, null);
     }
 
     @Override
     public void consume(String input) {
-        if (input.equalsIgnoreCase(this.answer)) {
+        if (input.equalsIgnoreCase("hint")) {
+            System.out.printf("Hint: %s%n", this.hint == null ? "None" : this.hint);
+        } else if (input.equalsIgnoreCase(this.answer)) {
             this.room.setCleared();
             this.room.chooseRoom();
         } else {
