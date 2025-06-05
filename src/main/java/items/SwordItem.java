751fa60f4024fb391e7d4a0e34688d19e2aed531
Julius Schutte <julius.schutte@gmail.com>
2025-06-05 11:49:23 +0200

    Added base items

---
+++ src/main/java/items/SwordItem.java
@@ -0,0 +1,22 @@
+package items;
+
+import core.Player;
+import rooms.Room;
+import rooms.TaskRoomWithMonster;
+
+public class SwordItem implements Item {
+    @Override
+    public void use(Player player, Room room) {
+        if (canUse(room)) { room.setCleared(); player.takeItem(this); }
+        else System.out.println("Nothing happened...");
+    }
+
+    private boolean canUse(Room room) {
+        return (room instanceof TaskRoomWithMonster roomWithMonster && roomWithMonster.getMonster().isActive());
+    }
+
+    @Override
+    public String getName() {
+        return "Sword of Monster Smashing";
+    }
+}
