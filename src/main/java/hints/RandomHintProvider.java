6486e0a840d61db831769a59bba673ae3192a704
TCAGM-Dev <thomas@andringa.org>
2025-06-06 13:38:34 +0200

    Fix RandomHintProvider

--- src/main/java/hints/RandomHintProvider.java
+++ src/main/java/hints/RandomHintProvider.java
@@ -1,23 +1,15 @@
 package hints;
 
-import core.DataSeeder;
-import rooms.Room;
-import java.util.List;
 import java.util.Random;
 
-public class RandomHintProvider {
-    private List<FunctionalHintProvider> functionalHints = DataSeeder.getFunctionalHints();
-    private List<LiteralHintProvider> uselessHints = DataSeeder.getUselessHints();
+public class RandomHintProvider implements HintProvider {
+    private final HintProvider[] providers;
 
-    public HintProvider getHint(Room room) {
-        Random random = new Random();
-        if (random.nextBoolean()) {
-            return this.uselessHints.get(random.nextInt(this.uselessHints.size()));
-        } else {
-            for (FunctionalHintProvider hint : this.functionalHints) {
-                if (hint.getRoom().equals(room)) return hint;
-            }
-        }
-        return null;
+    public RandomHintProvider(HintProvider[] providers) {
+        this.providers = providers;
+    }
+
+    public String getHint() {
+        return this.providers[new Random().nextInt(this.providers.length)].getHint();
     }
 }
