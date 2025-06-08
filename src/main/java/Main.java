c178a429631e511d6970d186d77d4b7a993908f0
TCAGM-Dev <thomas@andringa.org>
2025-06-08 13:15:57 +0200

    Create new DataSaver for use

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import core.Game;
+import saving.JSONDataSaver;
+
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String [] args) {
-        new Game(System.in).start();
+        new Game(System.in, new JSONDataSaver(Path.of(System.getProperty("user.dir")).resolve("./saves"))).start();
     }
 }
