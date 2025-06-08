cbb20b835847cd0c21cf85c098255799aa82616e
Julius Schutte <julius.schutte@gmail.com>
2025-06-09 01:11:11 +0200

    Added Assistant classes

---
+++ src/main/java/commands/commandslist/AssistCommand.java
@@ -0,0 +1,53 @@
+package commands.commandslist;
+
+import commands.Command;
+import commands.CommandManager;
+import core.Player;
+import items.consumables.HintJoker;
+
+import java.util.Random;
+
+public class AssistCommand implements Command {
+    private int uses = 2;
+    private static final String[] MOTIVATION_TEXT = { "Im proud of you", "You can do it", "I believe in you" };
+    private static final String[] MOTIVATIONAL_PLAN = {"google the answer", "cheat by using \"Up, Up, Down, Down, Left, Right, Left, Right, B, A,\""};
+    private static final Random RANDOM = new Random();
+
+    @Override
+    public void run(CommandManager commandManager, String args) {
+        if (this.uses > 0) {
+            HintJoker jokerItem = new HintJoker();
+            if (this.jokerUsed(commandManager, jokerItem)) commandManager.getParent().getPlayer().takeItem(jokerItem);
+            else { this.uses-- ; this.printExtraMotivation(); }
+        } else this.removeCommand(commandManager);
+    }
+
+    private void removeCommand(CommandManager commandManager) {
+        commandManager.removeCommand("assist");
+        System.out.println("Invalid Command");
+    }
+
+    private boolean jokerUsed(CommandManager commandManager, HintJoker jokerItem) {
+        Player player = commandManager.getParent().getPlayer();
+        int initialCount = player.getInventory().getOrDefault(jokerItem, 0);
+
+        player.giveItem(jokerItem);
+        commandManager.executeCommand("use Clue Joker");
+
+        return player.getInventory().getOrDefault(jokerItem, 0) > initialCount;
+    }
+
+    private void printExtraMotivation() {
+        System.out.println("[Assistant] " + MOTIVATIONAL_PLAN[RANDOM.nextInt(0, MOTIVATIONAL_PLAN.length)]);
+        System.out.println("[Assistant] " + MOTIVATION_TEXT[RANDOM.nextInt(0, MOTIVATION_TEXT.length)]);
+    }
+
+    public void setUses(int uses) {
+        this.uses = uses;
+    }
+
+    @Override
+    public String getKeyWord() {
+        return "assist";
+    }
+}
---
+++ src/main/java/entities/AssistantEntity.java
@@ -0,0 +1,30 @@
+package entities;
+
+import dialogue.DialogueManager;
+import events.Event;
+
+public class AssistantEntity implements DialogueEntity {
+    private DialogueManager dialogueManager = new DialogueManager("[Wooden doll] Hello there", this);
+    private Event parent;
+
+    public void consume(String input) {
+        this.dialogueManager.consume(input);
+    }
+
+    public void activate() {
+        this.dialogueManager.start();
+    }
+
+    @Override
+    public void endDialogue() {
+        this.parent.end();
+    }
+
+    public void setParent(Event parent) {
+        this.parent = parent;
+    }
+
+    public DialogueManager getDialogueManager() {
+        return this.dialogueManager;
+    }
+}
