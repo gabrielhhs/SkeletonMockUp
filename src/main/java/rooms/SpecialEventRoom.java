0151337abdbf814804c380d436ef799e77d324a6
Julius Schutte <julius.schutte@gmail.com>
2025-06-08 18:15:47 +0200

    Added SpecialEventRoom.java

---
+++ src/main/java/rooms/SpecialEventRoom.java
@@ -0,0 +1,28 @@
+package rooms;
+
+import core.Game;
+import events.Event;
+import events.EventHandler;
+
+public class SpecialEventRoom extends Room {
+    private final EventHandler eventHandler = new EventHandler(this);
+
+    public SpecialEventRoom(Game parent, String name, Event event) {
+        super(parent, name);
+        this.eventHandler.setEvent(event);
+    }
+
+    @Override
+    protected void onEnter() {
+        if (this.eventHandler.getEvent() != null && this.eventHandler.getEvent().isActiveOnEnter()) this.eventHandler.start();
+    }
+
+    @Override
+    protected void handleUncleared() {
+        //No existing uncleared tasks (possible)child classes should implement this
+    }
+
+    public EventHandler getEventHandler() {
+        return this.eventHandler;
+    }
+}
