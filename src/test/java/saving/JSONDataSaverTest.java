72e50030731b1d69af06e30f766836543c4d0527
ferrySoi <teg12toy@gmail.com>
2025-06-12 09:11:20 +0200

    this.

--- src/test/java/saving/JSONDataSaverTest.java
+++ src/test/java/saving/JSONDataSaverTest.java
@@ -1,7 +1,6 @@
 package saving;
 
 import core.Game;
-import core.GameStatus;
 import core.Player;
 import core.StatusManager;
 import items.Item;
@@ -41,33 +40,33 @@ class JSONDataSaverTest {
         MockitoAnnotations.openMocks(this);
 
         Map<Item, Integer> inventory = new HashMap<>();
-        inventory.put(mockItem1, 1);
-        inventory.put(mockItem2, 2);
+        inventory.put(this.mockItem1, 1);
+        inventory.put(this.mockItem2, 2);
 
-        when(mockGame.getPlayer()).thenReturn(mockPlayer);
-        when(mockPlayer.getInventory()).thenReturn(inventory);
-        when(mockRoom1.isCleared()).thenReturn(true);
-        when(mockRoom2.isCleared()).thenReturn(false);
-        when(mockRoom1.getName()).thenReturn("testRoom1");
-        when(mockRoom2.getName()).thenReturn("testRoom2");
-        when(mockGame.getStatusManager()).thenReturn(mockManager);
-        when(mockManager.get()).thenReturn(SELECTING_ROOM);
+        when(this.mockGame.getPlayer()).thenReturn(this.mockPlayer);
+        when(this.mockPlayer.getInventory()).thenReturn(inventory);
+        when(this.mockRoom1.isCleared()).thenReturn(true);
+        when(this.mockRoom2.isCleared()).thenReturn(false);
+        when(this.mockRoom1.getName()).thenReturn("testRoom1");
+        when(this.mockRoom2.getName()).thenReturn("testRoom2");
+        when(this.mockGame.getStatusManager()).thenReturn(this.mockManager);
+        when(this.mockManager.get()).thenReturn(SELECTING_ROOM);
 
-        Set<Room> rooms = new HashSet<>(Arrays.asList(mockRoom1, mockRoom2));
-        when(mockGame.collectRooms()).thenReturn(rooms);
+        Set<Room> rooms = new HashSet<>(Arrays.asList(this.mockRoom1, this.mockRoom2));
+        when(this.mockGame.collectRooms()).thenReturn(rooms);
 
-        saver = new JSONDataSaver(tempDir);
+        this.saver = new JSONDataSaver(this.tempDir);
     }
 
     @Test
     void save_shouldWriteToFile() throws Exception {
-        when(mockPlayer.getHealth()).thenReturn(75);
-        when(mockPlayer.getScore()).thenReturn(10);
-        when(mockPlayer.getCurrentRoom()).thenReturn(mockRoom1);
+        when(this.mockPlayer.getHealth()).thenReturn(75);
+        when(this.mockPlayer.getScore()).thenReturn(10);
+        when(this.mockPlayer.getCurrentRoom()).thenReturn(this.mockRoom1);
 
-        saver.save(mockGame, "testSave");
+        this.saver.save(this.mockGame, "testSave");
 
-        Path saveFile = tempDir.resolve("testSave.json");
+        Path saveFile = this.tempDir.resolve("testSave.json");
         assertTrue(Files.exists(saveFile));
 
         String content = Files.readString(saveFile);
@@ -76,25 +75,25 @@ class JSONDataSaverTest {
     }
 
     @Test
-    void save_shouldThrowSaverExceptionOnIOExeption() throws Exception {
+    void save_shouldThrowSaverExceptionOnIOExeption() {
         try (MockedStatic<Files> mockedFiles = mockStatic(Files.class)) {
             mockedFiles.when(() -> Files.createDirectory(any(Path.class)))
                     .thenThrow(new IOException("Test exception"));
 
-            assertThrows(SaverException.class, () -> new JSONDataSaver(tempDir));
+            assertThrows(SaverException.class, () -> new JSONDataSaver(this.tempDir));
         }
     }
 
     @Test
     void serialize_shouldIncludePlayerData() throws Exception {
-        when(mockPlayer.getCurrentRoom()).thenReturn(mockRoom1);
-        when(mockRoom1.getName()).thenReturn("testRoom1");
-        when(mockPlayer.getHealth()).thenReturn(100);
-        when(mockPlayer.getScore()).thenReturn(10);
+        when(this.mockPlayer.getCurrentRoom()).thenReturn(this.mockRoom1);
+        when(this.mockRoom1.getName()).thenReturn("testRoom1");
+        when(this.mockPlayer.getHealth()).thenReturn(100);
+        when(this.mockPlayer.getScore()).thenReturn(10);
 
-        saver.save(mockGame, "testSave");
+        this.saver.save(this.mockGame, "testSave");
 
-        Path saveFile = tempDir.resolve("testSave.json");
+        Path saveFile = this.tempDir.resolve("testSave.json");
         String jsonContent = Files.readString(saveFile);
         JSONObject json = new JSONObject(jsonContent);
         JSONObject playerJson = json.getJSONObject("player");
@@ -105,22 +104,22 @@ class JSONDataSaverTest {
     }
 
     @Test
-    void serialize_shouldBeTrueRoomClear() throws Exception{
-        when(mockPlayer.getCurrentRoom()).thenReturn(mockRoom1);
-        when(mockRoom1.isCleared()).thenReturn(true);
-        when(mockRoom2.isCleared()).thenReturn(false);
-        when(mockPlayer.getHealth()).thenReturn(100);
-        when(mockPlayer.getScore()).thenReturn(10);
+    void serialize_shouldBeTrueRoomClear() throws Exception {
+        when(this.mockPlayer.getCurrentRoom()).thenReturn(this.mockRoom1);
+        when(this.mockRoom1.isCleared()).thenReturn(true);
+        when(this.mockRoom2.isCleared()).thenReturn(false);
+        when(this.mockPlayer.getHealth()).thenReturn(100);
+        when(this.mockPlayer.getScore()).thenReturn(10);
 
-        saver.save(mockGame, "testSave");
+        this.saver.save(this.mockGame, "testSave");
 
-        Path saveFile = tempDir.resolve("testSave.json");
+        Path saveFile = this.tempDir.resolve("testSave.json");
         String jsonContent = Files.readString(saveFile);
         JSONObject json = new JSONObject(jsonContent);
         JSONObject roomJson = json.getJSONObject("roomClears");
 
-        assertTrue(roomJson.getBoolean(mockRoom1.getName()));
-        assertFalse(roomJson.getBoolean(mockRoom2.getName()));
+        assertTrue(roomJson.getBoolean(this.mockRoom1.getName()));
+        assertFalse(roomJson.getBoolean(this.mockRoom2.getName()));
     }
 
     @Test
@@ -139,47 +138,45 @@ class JSONDataSaverTest {
                         "inventory": {}
                     }
                 }""";
-        Path saveFile = tempDir.resolve("testLoad.json");
-        when(mockPlayer.getCurrentRoom()).thenReturn(mockRoom1);
+        Path saveFile = this.tempDir.resolve("testLoad.json");
+        when(this.mockPlayer.getCurrentRoom()).thenReturn(this.mockRoom1);
         Files.writeString(saveFile, testData);
 
+        this.saver.load(this.mockGame, "testLoad");
 
-        saver.load(mockGame, "testLoad");
-
-        verify(mockRoom1).setCleared(true);
-        verify(mockRoom2).setCleared(false);
-
-        verify(mockPlayer).setCurrentRoom(mockRoom1);
-        verify(mockManager).set(SELECTING_ROOM);
-        verify(mockPlayer).setHealth(100);
-        verify(mockPlayer).setScore(10);
-        verify(mockRoom1).enter();
+        verify(this.mockRoom1).setCleared(true);
+        verify(this.mockRoom2).setCleared(false);
+        verify(this.mockPlayer).setCurrentRoom(this.mockRoom1);
+        verify(this.mockManager).set(SELECTING_ROOM);
+        verify(this.mockPlayer).setHealth(100);
+        verify(this.mockPlayer).setScore(10);
+        verify(this.mockRoom1).enter();
     }
 
     @Test
-    void saveExists_shouldExist() throws Exception {
-        when(mockPlayer.getCurrentRoom()).thenReturn(mockRoom1);
-        when(mockRoom1.isCleared()).thenReturn(true);
-        when(mockRoom2.isCleared()).thenReturn(false);
-        when(mockPlayer.getHealth()).thenReturn(100);
-        when(mockPlayer.getScore()).thenReturn(10);
+    void saveExists_shouldExist() {
+        when(this.mockPlayer.getCurrentRoom()).thenReturn(this.mockRoom1);
+        when(this.mockRoom1.isCleared()).thenReturn(true);
+        when(this.mockRoom2.isCleared()).thenReturn(false);
+        when(this.mockPlayer.getHealth()).thenReturn(100);
+        when(this.mockPlayer.getScore()).thenReturn(10);
 
-        saver.save(mockGame, "testSave");
+        this.saver.save(this.mockGame, "testSave");
 
-        assertTrue(saver.saveExists("testSave"));
+        assertTrue(this.saver.saveExists("testSave"));
     }
 
     @Test
-    void saveExist_shouldNotExist() throws Exception {
-        assertFalse(saver.saveExists("testSave"));
+    void saveExist_shouldNotExist() {
+        assertFalse(this.saver.saveExists("testSave"));
     }
 
     @Test
     void getSaves_shouldReturnSaveNames() throws Exception {
-        Files.createFile(tempDir.resolve("testSave1.json"));
-        Files.createFile(tempDir.resolve("testSave2.json"));
+        Files.createFile(this.tempDir.resolve("testSave1.json"));
+        Files.createFile(this.tempDir.resolve("testSave2.json"));
 
-        Set<String> saves = saver.getSaves();
+        Set<String> saves = this.saver.getSaves();
 
         assertTrue(saves.contains("testSave1.json"));
         assertTrue(saves.contains("testSave2.json"));
